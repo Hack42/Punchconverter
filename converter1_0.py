4f9300d673e48349e4c35b636e8e1e0cbfc0d581
Simon Claessen <info@drukknop.nl>
2013-10-28 11:32:27 +0100

    further progress

--- converter1_0.py
+++ converter1_0.py
@@ -19,21 +19,51 @@ fp=flexo president
 tsa=typesetter 6bit version a
 tsb=typesetter version b
 tx=ita2 telex""",required=True)
-    args = parser.parse_args()
-    
-    print("Input file: %s" % args.input )
-    print("format: %s" % args.format )
+	parser.add_argument('-p','--port', help='serial port',required=True)
+args = parser.parse_args()
+
+#some feedback
+print("Input file: %s" % args.input )
+print("format: %s" % args.format )
 
 
-    # setup serial port
-    port = serial.Serial(0) # open serial port
-    port.baudrate = 1200
-    print port.name
+# setup serial port
+port = serial.Serial(0) # open serial port
+port.baudrate = 1200
+print port.name
     
-    with open(args.input) as f:
-	    while True:
-		    c= f.read(1)
-		    if not c:
-			    print "EOF"
-			    break
+#read single chars from source file and parse them	
+with open(args.input) as f:
+	while True:
+		c= f.read(1)
+		if not c:
+			print "EOF"
+			break
 		
+parseto = {@ : escapechar,
+					1 : sqr,
+					
+}
+
+# convert between escapechars in the format @@xyz. if no solution is found, print all the chars
+# including @@
+def escapechar():
+		c=f.read(1)
+		if c == '@'
+			c=f.read(3)
+			if c == 'str':
+				c=0x19
+			elif c == 'swr':
+				c=0x1F
+			elif c == 'lcs':
+				c =0x37
+			elif c == 'ucs':
+				c=0x57
+			elif c == 'pof':
+				c=0x67
+			else pushout('@')
+			
+		else pushout('@')
+				pushout(c)
+			
+			
