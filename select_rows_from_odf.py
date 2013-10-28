46ef97e5251d635fde083bf3ec8bff0a818aa566
Simon Claessen <info@drukknop.nl>
2013-10-28 14:48:52 +0100

    updated odf stuff

--- select_rows_from_odf.py
+++ select_rows_from_odf.py
@@ -6,5 +6,5 @@ with open("punchtape.csv","rb") as source:
     with open("result.csv","wb") as result:
         wtr= csv.writer( result )
         for r in rdr:
-		if r[14]:
-			wtr.writerow( (r[1], r[2], r[14], r[15]) )
+		if r[12]:
+			wtr.writerow( (r[1], r[2], r[12], r[13]) )
